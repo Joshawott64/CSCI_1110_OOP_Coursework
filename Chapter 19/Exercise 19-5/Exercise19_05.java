b8b5d749af2126985a9c7dcaabee09ca5c1e00d0
Joshua Gray <86368706+Joshawott64@users.noreply.github.com>
2021-09-10 09:46:06 -0600

    Added Exercise 19-5 folder and Exercise19_05.java

---
+++ Chapter 19/Exercise 19-5/Exercise19_05.java
@@ -0,0 +1,55 @@
+
+public class Exercise19_05 {
+
+	public static void main(String[] args) {
+		Integer[] numbers = {1, 2, 3};
+		System.out.println(max(numbers));
+		
+		String[] words = {"red", "green", "blue"};
+		System.out.println(max(words));
+		
+		Circle[] circles = {new Circle(3), new Circle(2.9), new Circle(5.9)};
+		System.out.println(max(circles));
+	}
+	
+	static class Circle implements Comparable<Circle> {
+		double radius;
+		
+		public Circle(double radius) {
+			this.radius = radius;
+		}
+		@Override
+		public int compareTo(Circle c) {
+			if (radius < c.radius) {
+				return -1;
+			}
+			else if (radius == c.radius ) {
+				return 0;
+			}
+			else {
+				return 1;
+			}
+		}
+		@Override
+		public String toString() {
+			return ("Circle radius: " + radius);
+		}
+	}
+	
+	public static <E extends Comparable<E>> E max(E[] list) {
+		E currentMax = null;
+		
+		for (int i = 0; i < list.length - 1; i++) {
+			// Find maximum in list
+			currentMax = list[i];
+			
+			for (int j = 0; j < list.length; j++) {
+				if (currentMax.compareTo(list[j]) < 0) {
+					currentMax = list[j];
+				}
+			}
+		}
+		return currentMax;
+	}
+
+}
